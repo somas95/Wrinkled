348d8e9dd9c46bae0ef5b881411bb71dac7a7c49
Manuel Genovés <manuel.genoves@gmail.com>
2020-05-04 19:23:19 +0200

    Initial commit

---
+++ Assets/Scripts/FPSCameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSCameraController : MonoBehaviour
+{
+    public Camera mainCamera;
+    public float horizontalSpeed;
+    public float verticalSpeed;
+
+    float h, v;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        h = horizontalSpeed * Input.GetAxis("Mouse X");
+        v = verticalSpeed * Input.GetAxis("Mouse Y");
+
+        transform.Rotate(0,h,0);
+        mainCamera.transform.Rotate(v, 0, 0);
+        
+    }
+}
---
+++ Assets/Scripts/Player3DController.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player3DController : MonoBehaviour
+{
+
+    private float horizontalMov;
+    private float verticalMov;
+
+    private Vector3 playerInput;
+    private Vector3 movePlayer;
+
+    public Camera mainCamera;
+    private Vector3 camForward;
+    private Vector3 camRight;
+
+
+    public CharacterController player;
+    public float speed;
+    public float gravity = -9.8f;
+    public float jumpForce;
+    private float y_velocity = -1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<CharacterController>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMov = Input.GetAxis("Horizontal");
+        verticalMov = Input.GetAxis("Vertical");
+
+        playerInput = new Vector3(horizontalMov, 0, verticalMov);   //normalizamos
+        playerInput = Vector3.ClampMagnitude(playerInput, 1);
+
+        camDirection();
+
+        movePlayer = playerInput.x * camRight + playerInput.z * camForward;
+        movePlayer *= speed;
+        //player.transform.LookAt(player.transform.position + movePlayer);
+
+        setGravity();
+
+        advancedMovement();
+
+        player.Move(movePlayer * Time.deltaTime);
+        
+        
+    }
+
+    void camDirection()
+    {
+        camForward = mainCamera.transform.forward;
+        camRight = mainCamera.transform.right;
+
+        camForward.y = 0;
+        camRight.y = 0;
+
+        camForward = camForward.normalized;
+        camRight = camRight.normalized;
+    }
+
+    void advancedMovement()
+    {
+        if (player.isGrounded && Input.GetButtonDown("Jump"))
+        {
+            y_velocity = jumpForce;
+            movePlayer.y = y_velocity;
+        }
+    }
+
+    void setGravity()
+    {
+        if (player.isGrounded)
+        {
+            movePlayer.y = -1;
+        } else {
+            y_velocity += gravity * Time.deltaTime;
+            movePlayer.y = y_velocity; 
+        }
+    }
+
+
+}
